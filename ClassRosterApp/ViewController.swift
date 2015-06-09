644656a57306f2bc09382e27932b70c2b59610d2
Alberto-Vega <el_vega@live.com>
2015-06-08 22:15:42 -0700

    First assignment added 2 buttons and a label

--- ClassRosterApp/ViewController.swift
+++ ClassRosterApp/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  @IBOutlet weak var studentNameLabel: UILabel!
+  
+  let students = ["Heidi", "Joao", "Alberto", "Richard", "Karinne"]
+  var currentIndex = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    studentNameLabel.text = self.students[0]
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func nextButtonPressed(sender: UIButton) {
+    if currentIndex < students.count - 1 {
+      currentIndex += 1
+      println(self.currentIndex)
+    } else {
+      currentIndex = students.count - 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let studentName = students[currentIndex]
+    studentNameLabel.text = studentName
+  }
+  
+  @IBAction func previousButtonPressed(sender: UIButton) {
+    if currentIndex > 0 {
+      currentIndex -= 1
+      println(self.currentIndex)
+    } else {
+      currentIndex = 0
     }
-
-
+    let studentName = students[currentIndex]
+    studentNameLabel.text = studentName
+  }
 }
 
